5a7a554ee513ca58bd1e17694c3428b05508fbdf
Andreas <andreas@andreasgassmann.ch>
2017-09-16 04:53:35 +0200

    feat(contacts): add mock data in browser

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
 import { Contacts, IContactFindOptions, ContactFindOptions, Contact, ContactField, ContactFieldType, ContactName } from '@ionic-native/contacts';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -10,30 +10,50 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class ProfilePage {
   myContacts: any;
 
-  constructor(public navCtrl: NavController, private contacts: Contacts, private sanitizer: DomSanitizer) {
-    let fields: ContactFieldType[] = ["displayName", "addresses"];
+  constructor(public navCtrl: NavController, private contacts: Contacts, private sanitizer: DomSanitizer, private platform: Platform) {
+    if (this.platform.is('cordova')) {
+      let fields: ContactFieldType[] = ["displayName", "addresses"];
 
-    let options: IContactFindOptions = new ContactFindOptions();
-    options.filter = "";
-    options.multiple = true;
-    options.hasPhoneNumber = true;
+      let options: IContactFindOptions = new ContactFindOptions();
+      options.filter = "";
+      options.multiple = true;
+      options.hasPhoneNumber = true;
 
-    this.contacts.find(fields, options).then((contacts) => {
-      console.log(contacts);
-      this.myContacts = contacts.filter(c => c.phoneNumbers).map(c => {
-        return {
-          name: c.name.formatted,
-          phoneNumber: c.phoneNumbers[0].value,
-          photo: c.photos ? c.photos[0].value : ''
-        }
+      this.contacts.find(fields, options).then((contacts) => {
+        console.log(contacts);
+        this.myContacts = contacts.filter(c => c.phoneNumbers).map(c => {
+          return {
+            name: c.name.formatted,
+            phoneNumber: c.phoneNumbers[0].value,
+            photo: c.photos ? c.photos[0].value : ''
+          }
+        });
+        console.log(this.myContacts);
       });
-      console.log(this.myContacts);
-    });
+    } else {
+      this.myContacts = [{
+        name: 'Andy',
+        phoneNumber: '+41 12 345 67 89',
+        photo: 'https://unsplash.it/200'
+      }, {
+        name: 'Pascal',
+        phoneNumber: '+41 12 345 67 89',
+        photo: 'https://unsplash.it/201'
+      },
+      {
+        name: 'Ale',
+        phoneNumber: '+41 12 345 67 89',
+        photo: 'https://unsplash.it/202'
+      }];
+    }
   }
 
   sanitizeImage(value) {
-    console.log(value.substr(77));
-    return this.sanitizer.bypassSecurityTrustUrl('file://' + value)
+    if (this.platform.is('cordova')) {
+      return this.sanitizer.bypassSecurityTrustUrl('file://' + value)
+    } else {
+      return value;
+    }
   }
 
 }
